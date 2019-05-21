08cccede4b01d4e1822fcc8a2fc9a80dcdb4ec85
sneha <lakshmisneha.516@gmail.com>
2019-05-21 14:04:56 +0530

    keepnote 1 completed

---
+++ KeepNote-Step1-Boilerplate/src/main/java/com/stackroute/keepnote/model/Note.java
@@ -0,0 +1,85 @@
+package com.stackroute.keepnote.model;
+
+import java.time.LocalDateTime;
+
+/*
+ * The class "Note" will be acting as the data model for the Note data in the ArrayList.
+ */
+public class Note {
+
+	/*
+	 * This class should have five fields (noteId, noteTitle, noteContent,
+	 * noteStatus and createdAt). This class should also contain the getters and
+	 * setters for the fields. The value of createdAt should not be accepted from
+	 * the user but should be always initialized with the system date
+	 */
+
+	private int noteId;
+	private String noteTitle;
+	private  String noteContent;
+	private String noteStatus;
+	private LocalDateTime createdAt;
+
+	public Note() {
+	}
+
+	/* All the getters/setters definition should be implemented here */
+
+	public int getNoteId() {
+		return this.noteId;
+
+	}
+
+	public void setNoteId(int intid) {
+		this.noteId=intid;
+
+	}
+
+	public String getNoteTitle() {
+		return this.noteTitle;
+	}
+
+	public void setNoteTitle(String string) {
+		this.noteTitle=string;
+
+	}
+
+	public String getNoteContent() {
+
+		return this.noteContent;
+	}
+
+	public void setNoteContent(String string) {
+
+		this.noteContent=string;
+	}
+
+	public String getNoteStatus() {
+
+		return this.noteStatus;
+	}
+
+	public void setNoteStatus(String string) {
+
+			this.noteStatus = string;
+	}
+
+	public LocalDateTime getCreatedAt() {
+
+		return this.createdAt;
+	}
+
+	public void setCreatedAt(LocalDateTime localdatetime) {
+
+		this.createdAt=localdatetime;
+	}
+
+	/* Override the toString() method */
+
+	@Override
+	public String toString() {
+
+		return this.noteId + " " + this.noteTitle + " " + this.noteContent + " " + this.noteStatus + " "
+				+ this.createdAt;
+	}
+}
---
+++ KeepNote-Step1-Boilerplate/src/main/java/com/stackroute/keepnote/repository/NoteRepository.java
@@ -0,0 +1,95 @@
+package com.stackroute.keepnote.repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.stackroute.keepnote.model.Note;
+
+/*
+ * This class contains the code for data storage interactions and methods 
+ * of this class will be used by other parts of the applications such
+ * as Controllers and Test Cases
+ * */
+
+public class NoteRepository {
+
+	/* Declare a variable called "list" to store all the notes. */
+	ArrayList<Note> list = new ArrayList<Note>();
+
+	public NoteRepository() {
+
+		/* Initialize the variable using proper data type */
+	}
+
+	/* This method should return all the notes in the list */
+
+	public List<Note> getList() {
+
+		return this.list;
+	}
+
+	/* This method should set the list variable with new list of notes */
+
+	public void setList(List<Note> list) {
+
+		this.list = (ArrayList<Note>) list;
+
+	}
+
+	/*
+	 * This method should Note object as argument and add the new note object into
+	 * list
+	 */
+
+	public void addNote(Note note) {
+
+			list.add(note);
+	}
+
+	/* This method should deleted a specified note from the list */
+
+	public boolean deleteNote(int noteId) {
+		/* Use list iterator to find matching note id and remove it from the list */
+		boolean delete = false;
+		int indexOfId = 0;
+		for (Note note : list) {
+			int noteID = note.getNoteId();
+			if (noteID == noteId) {
+				list.remove(indexOfId);
+				delete = true;
+				break;
+			} else {
+				delete = false;
+				indexOfId++;
+			}
+		}
+
+		return delete;
+
+
+	}
+
+	/* This method should return the list of notes */
+
+	public List<Note> getAllNotes() {
+
+		return this.list;
+	}
+
+	/*
+	 * This method should check if the matching note id present in the list or not.
+	 * Return true if note id exists in the list or return false if note id does not
+	 * exists in the list
+	 */
+
+	public boolean exists(int noteId) {
+		boolean exist = false;
+			for (Note note:list) {
+				if (note.getNoteId() == noteId) {
+					return true;
+				}
+			}
+					return exist;
+	}
+
+}
